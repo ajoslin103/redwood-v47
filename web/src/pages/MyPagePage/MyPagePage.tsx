8ab60ae75e64768a3f9952d89e01235cd25e4b12
Al Joslin <allen.joslin@gmail.com>
2022-02-27 10:59:09 -0500

    display the consumption

--- web/src/pages/MyPagePage/MyPagePage.tsx
+++ web/src/pages/MyPagePage/MyPagePage.tsx
@@ -1,7 +1,15 @@
 import { Link, routes } from '@redwoodjs/router'
 import { MetaTags } from '@redwoodjs/web'
 
+import RunQueryCell from 'src/components/RunQueryCell'
+import RunMutationCell from 'src/components/RunMutationCell'
+
 const MyPagePage = () => {
+
+  const doComplete = () => {}
+
+  const doError = () => {}
+
   return (
     <>
       <MetaTags title="MyPage" description="MyPage page" />
@@ -10,6 +18,8 @@ const MyPagePage = () => {
       <p>
         Find me in <code>./web/src/pages/MyPagePage/MyPagePage.tsx</code>
       </p>
+      <RunQueryCell />
+      <RunMutationCell onComplete={doComplete} onError={doError} />
       <p>
         My default route is named <code>myPage</code>, link to me with `
         <Link to={routes.myPage()}>MyPage</Link>`
